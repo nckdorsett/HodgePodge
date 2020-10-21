a31e4a28e9f72d052f18f431b9ac9c9c8f469fbe
Nicholas Dorsett <ndorsett@dev-10.com>
2020-10-21 11:21:41 -0500

    mapping added, .http file made

--- src/main/java/learn/hodge/controllers/HodgePodgeController.java
+++ src/main/java/learn/hodge/controllers/HodgePodgeController.java
@@ -1,9 +1,47 @@
 package learn.hodge.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @RestController
 public class HodgePodgeController {
 
-    
+    static int sheepCount = 0;
+
+    @GetMapping("/{name}")
+    public String getName(@PathVariable String name) {
+        return "your name " + name;
+    }
+
+    @GetMapping("/current/time")
+    public LocalDateTime getTime() {
+        return LocalDateTime.now();
+    }
+
+    @GetMapping("/greet/{name}")
+    public String getGreeting(@PathVariable String name) {
+        return "Hello, " + name;
+    }
+
+    @PutMapping("/sheep")
+    public void putSheep() {
+        sheepCount++;
+        System.out.println(sheepCount);
+    }
+
+    @GetMapping("/sheep")
+    public int getSheepCount() {
+        return sheepCount;
+    }
+
+    @PutMapping("/sheep/{amount}")
+    public void increaseSheep(@PathVariable int amount) {
+        sheepCount = sheepCount + amount;
+    }
+
 }
